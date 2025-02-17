aa24be72f5f388b220bcff9bcf8072f496321329
davelin18yufan <davelin30630@gmail.com>
2025-02-17 21:18:54 +0800

    [feat] add pg SDK and connected.

---
+++ src/database/database.ts
@@ -0,0 +1,25 @@
+import pg from "pg"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const { Pool } = pg
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER || "postgres",
+  password: process.env.POSTGRES_PASSWORD || "postgres",
+  host: process.env.POSTGRES_HOST || "localhost",
+  port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
+  database: process.env.POSTGRES_DB || "postgres",
+  max: 20, // Maximum number of clients in the pool
+  idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+  connectionTimeoutMillis: 2000, // How long to wait for a connection
+})
+
+// The pool will emit an error on behalf of any idle clients
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err)
+  process.exit(-1)
+})
+
+export default pool
